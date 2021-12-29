334602345531ebc121c2eb6a5d9133d3cae238cb
Liu, Xin <Xin.Liu@cn.yokogawa.com>
2021-12-29 16:01:33 +0800

    Add project files.

---
+++ wincronjob/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.VisualBasic;
+
+
+
+namespace wincronjob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var token = Environment.GetEnvironmentVariable("TOKEN");
+                using var httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var response = httpClient.GetStringAsync("https://broker.msa.apps.yokogawa.dev/resources/ashok-test-29/events").Result;
+                Console.WriteLine(response);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+        }
+    }
+}
