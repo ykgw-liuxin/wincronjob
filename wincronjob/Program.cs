f5eb6dcc8f7720d6d9e1b73449e3d471536ff7a9
Liu, Xin <Xin.Liu@cn.yokogawa.com>
2021-12-30 10:41:34 +0800

    1

--- wincronjob/Program.cs
+++ wincronjob/Program.cs
@@ -13,29 +13,24 @@ namespace wincronjob
     {
         static void Main(string[] args)
         {
-            Print("1");
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("1");
+            //    Thread.Sleep(1000);
+            //}
+
             try
             {
                 var token = Environment.GetEnvironmentVariable("TOKEN");
                 using var httpClient = new HttpClient();
                 httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
                 var response = httpClient.GetStringAsync("https://broker.msa.apps.yokogawa.dev/resources/ashok-test-29/events").Result;
-                Console.WriteLine(response);
+                Console.WriteLine(response.Substring(0, Math.Min(1024, response.Length)));
 
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error: " + ex);
-            }
-            //Print("2");
-        }
-
-        static void Print(string msg)
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(msg);
-                Thread.Sleep(1000);
             }
         }
     }
